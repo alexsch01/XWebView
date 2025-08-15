24cbfb580c0a3d728d9168c681f9df6436c8e29d
Alex Schwartz <alexschwartz01@gmail.com>
2025-08-15 09:48:43 -0400

    fullscreen video workaround

--- app/src/main/java/com/alexsch01/twitter/MainActivity.kt
+++ app/src/main/java/com/alexsch01/twitter/MainActivity.kt
@@ -3,11 +3,17 @@ package com.alexsch01.twitter
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myWebView: WebView
@@ -41,6 +47,30 @@ class MainActivity : AppCompatActivity() {
                     website.toUri()
                 ))
                 return true
+            }
+        }
+
+        // Workaround for fullscreen videos
+        myWebView.webChromeClient = object : WebChromeClient() {
+            private val frameLayout: FrameLayout = findViewById(R.id.frameLayout)
+            private val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+                // get into proper fullscreen mode
+                insetsController.systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                insetsController.hide(WindowInsetsCompat.Type.systemBars())
+
+                frameLayout.addView(view, 1)
+            }
+
+            override fun onHideCustomView() {
+                frameLayout.removeViewAt(1)
+
+                // get out of proper fullscreen mode
+                insetsController.systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+                insetsController.show(WindowInsetsCompat.Type.systemBars())
             }
         }
 
